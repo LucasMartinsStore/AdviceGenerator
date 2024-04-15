caaf0e0747f829bcfc961c653a992ad16528d9fa
Lucas Martins <lucasifbaeuna@gmail.com>
2024-04-15 13:08:49 -0300

    fi: button disabled

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,16 +1,18 @@
 import { Slip } from './../../interface/slip.interface';
 import { Component, OnInit } from '@angular/core';
 import { AdviceService } from '../../shared/service/advice.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss',
 })
 export class CardComponent implements OnInit {
   slipList!: Slip;
+  isLoading = false;
 
   constructor(private adviceService: AdviceService) {}
   ngOnInit(): void {
@@ -18,9 +20,12 @@ export class CardComponent implements OnInit {
   }
 
   getAdvice() {
-    this.adviceService.getSlip().subscribe((data: Slip) => {
-      console.log(data);
-      this.slipList = data;
+    this.isLoading = true;
+    this.adviceService.getSlip().subscribe({
+      next: (data: Slip) => {
+        this.slipList = data;
+        this.isLoading = false;
+      },
     });
   }
 }
